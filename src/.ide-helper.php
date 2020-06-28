0a33780815735e77388af8c2c687068a457e1154
jqh <841324345@qq.com>
2020-06-28 09:47:00 +0800

    update

---
+++ src/.ide-helper.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Illuminate\Database\Eloquent
+{
+    /**
+     * @method $this whereHasIn(string $relation, ?\Closure $callable = null)
+     * @method $this orWhereHasIn(string $relation, ?\Closure $callable = null)
+     * @method $this whereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
+     * @method $this orWhereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
+     */
+    class Builder
+    {
+    }
+
+    /**
+     * @method static $this whereHasIn(string $relation, ?\Closure $callable = null)
+     * @method static $this orWhereHasIn(string $relation, ?\Closure $callable = null)
+     * @method static $this whereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
+     * @method static $this orWhereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
+     */
+    class Model
+    {
+    }
+}
