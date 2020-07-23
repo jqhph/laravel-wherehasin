4ba470ebad1978232832474653c6a25841cced02
Jiang Qinghua <841324345@qq.com>
2020-07-23 15:32:57 +0800

    Update .ide-helper.php

--- src/.ide-helper.php
+++ src/.ide-helper.php
@@ -11,14 +11,4 @@ namespace Illuminate\Database\Eloquent
     class Builder
     {
     }
-
-    /**
-     * @method static Builder whereHasIn(string $relation, ?\Closure $callable = null)
-     * @method static Builder orWhereHasIn(string $relation, ?\Closure $callable = null)
-     * @method static Builder whereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
-     * @method static Builder orWhereHasMorphIn(string $relation, $types, ?\Closure $callable = null)
-     */
-    class Model
-    {
-    }
 }
