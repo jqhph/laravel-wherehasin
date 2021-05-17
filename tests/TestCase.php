68f8688d58a66e583c928414b3e3ce4057e3bdc1
jqh <841324345@qq.com>
2021-05-17 17:00:38 +0800

    action

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,10 +19,10 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         $this->app['config']->set('database.default', 'mysql');
-        $this->app['config']->set('database.connections.mysql.host', env('MYSQL_HOST', 'localhost'));
-        $this->app['config']->set('database.connections.mysql.database', env('MYSQL_DATABASE', 'laravel_test'));
+        $this->app['config']->set('database.connections.mysql.host', env('MYSQL_HOST', '127.0.0.1'));
+        $this->app['config']->set('database.connections.mysql.database', env('MYSQL_DATABASE', 'laravel'));
         $this->app['config']->set('database.connections.mysql.username', env('MYSQL_USER', 'root'));
-        $this->app['config']->set('database.connections.mysql.password', env('MYSQL_PASSWORD', ''));
+        $this->app['config']->set('database.connections.mysql.password', env('MYSQL_PASSWORD', '123456'));
         $this->app['config']->set('app.key', 'AckfSECXIvnK5r28GVIWUAxmbBSjTsmF');
 
         Schema::defaultStringLength(191);
